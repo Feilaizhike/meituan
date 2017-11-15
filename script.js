5f7388f57ea648a96d8b2152e552d30b962b010e
feifei <1026835233@qq,com>
2017-11-15 11:16:07 +0800

    添加底部

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ window.onload=function(){
         autoplay: 3000,
         autoplayDisableOnInteraction: false
     });
-
-
 }
+
+/* 头部更多添加触碰事件 弹出二级菜单*/
 var header = document.getElementById("header_right"),
     headM = document.getElementById("header_more");
 
@@ -23,3 +23,28 @@ var header = document.getElementById("header_right"),
                 headM.style.display = "none";
             }
     },false);
+
+/* 底部菜单触碰事件*/
+/*
+var aList =document.getElementById('bottom_menu_li').getElementsByTagName('li');
+var aImg =document.getElementById('bottom_menu_li').getElementsByTagName('img');
+console.log(aImg)
+for( var i=0; i < aList.length; i++){
+    aList[i].index=i;
+    aList[i].onmousedown = function(){
+        var nIndex=this.index+1;
+
+       // aImg[this.index].src=" 'image/footer0'+this.index+1+'.1png' "
+        var myNSrc ='image/footer0'+nIndex+'.1.png';
+        var mySrc = 'image/footer0'+nIndex+'.png';
+        if(aImg[nIndex-1].src==myNSrc){
+            aImg[nIndex-1].src=mySrc;
+        }else{
+           aImg[nIndex-1].src = myNSrc;
+        }
+
+    }
+}*/
+
+
+
